93a5318a86d4bab3197ee789f6df3bbc889297b1
sojeonglee <sojeonglee0125@gmail.com>
2022-11-15 20:43:10 +0900

    feat : MessageCodesResolver 테스트 코드 작성

---
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,27 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObect() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        Assertions.assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        Assertions.assertThat(messageCodes).containsExactly("required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required");
+    }
+}
